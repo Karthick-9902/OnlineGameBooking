3d01398d6750cbeecc39dae2f512ae6baad895e3
Karthick-9902 <126456084+Karthick-9902@users.noreply.github.com>
2023-02-27 14:52:23 +0530

    Update JDBCUtil.java

--- BookMyGame/src/main/java/com/bmg/util/JDBCUtil.java
+++ BookMyGame/src/main/java/com/bmg/util/JDBCUtil.java
@@ -1,11 +1,11 @@
 package com.bmg.util;
 
-//import java.io.FileInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-//import java.sql.DriverManager;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-//import java.util.Properties;
+import java.util.Properties;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -23,18 +23,18 @@ public class JDBCUtil {
 	}
 	@SuppressWarnings("resource")
 	public static Connection getConnection()  throws SQLException, IOException{
-		String loc="D:\\Projects\\BookMyGame\\src\\main\\java\\com\\bmg\\dbproperty\\db.properties";
-	//	String loc="D:\\Projects\\BookMyGame\\src\\main\\java\\com\\bmg\\dbproperty\\db1.properties";
-	  HikariConfig hikariConfig= new HikariConfig(loc);
-	  HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
-	  Connection connection = hikariDataSource.getConnection();
-		/*
-		 * FileInputStream fis = new FileInputStream(loc); Properties properties = new
-		 * Properties(); properties.load(fis); String url =
-		 * properties.getProperty("url"); String user = properties.getProperty("user");
-		 * String password = properties.getProperty("password"); Connection connection =
-		 * DriverManager.getConnection(url, user, password);
-		 */
+	//	String loc="D:\\Projects\\BookMyGame\\src\\main\\java\\com\\bmg\\dbproperty\\db.properties";
+		String loc="D:\\Projects\\BookMyGame\\src\\main\\java\\com\\bmg\\dbproperty\\db1.properties";
+	//  HikariConfig hikariConfig= new HikariConfig(loc);
+	 // HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
+	 // Connection connection = hikariDataSource.getConnection();
+		
+		  FileInputStream fis = new FileInputStream(loc); Properties properties = new
+		  Properties(); properties.load(fis); String url =
+		  properties.getProperty("url"); String user = properties.getProperty("user");
+		  String password = properties.getProperty("password"); Connection connection =
+		  DriverManager.getConnection(url, user, password);
+		 
 		return connection;
 	}
 }
